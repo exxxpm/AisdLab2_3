bb6c6a02e00ade342b540e8826110f0888a508a0
exxxpm <127376127+exxxpm@users.noreply.github.com>
2024-05-29 09:59:19 +0400

    added degree, order

--- include/graph.cc
+++ include/graph.cc
@@ -94,4 +94,48 @@ public:
                                       std::fabs(edge.distance - ed.distance) < std::numeric_limits<double>::epsilon();
                            }); // Проверяем, есть ли ребро ed в контейнере _edges для вершины ed.from
     }
+
+    bool remove_edge(const V &from, const V &to) {
+        // Метод удаляет ребро между вершинами from и to
+        if (!_edges.contains(from) || !_vertices.contains(to))
+            return false; // Проверяем, существуют ли вершины from и to в графе
+        auto cnt_erased = 0; // Переменная для подсчета удаленных ребер
+        cnt_erased += std::erase_if(_edges[from],
+                                    [to](const Edge &edge) {
+                                        return edge.to == to;
+                                    }); // Удаляем ребро с вершиной "to" из контейнера _edges для вершины from
+        if (cnt_erased) return true; // Возвращаем true, если было удалено хотя бы одно ребро
+        return false; // Возвращаем false, если ребро не было найдено и удалено
+    }
+
+    bool remove_edge(const Edge &ed) {
+        // Метод удаляет ребро ed из графа
+        if (!_edges.contains(ed.from) || !_vertices.contains(ed.to))
+            return false; // Проверяем, существуют ли вершины from и to ребра ed в графе
+        auto cnt_erased = 0; // Переменная для подсчета удаленных ребер
+        cnt_erased += std::erase_if(_edges[ed.from],
+                                    [ed](const Edge &edge) {
+                                        return edge.to == ed.to &&
+                                               std::fabs(edge.distance - ed.distance) <
+                                               std::numeric_limits<double>::epsilon();
+                                    }); // Удаляем ребро ed из контейнера _edges для вершины ed.from
+        if (cnt_erased) return true; // Возвращаем true, если было удалено хотя бы одно ребро
+        return false; // Возвращаем false, если ребро не было найдено и удалено
+    }
+    std::vector<Edge> edges(const V &vertex) const {
+        // Метод возвращает вектор ребер, инцидентных заданной вершине vertex
+        if (!has_vertex(vertex)) return {}; // Если вершина не существует, возвращаем пустой вектор
+        return _edges.at(vertex); // Возвращаем вектор ребер, инцидентных вершине vertex
+    }
+
+    size_t order() const {
+        // Метод возвращает количество вершин в графе
+        return _edges.size(); // Возвращаем размер контейнера _edges, который представляет количество вершин
+    }
+
+    size_t degree(const V &v) const {
+        // Метод возвращает степень вершины v в графе
+        if (!has_vertex(v)) return 0; // Если вершина не существует, возвращаем 0
+        return _edges.at(v).size(); // Возвращаем размер вектора ребер, инцидентных вершине v
+    }
 };
--- tests/graph_tests.cc
+++ tests/graph_tests.cc
@@ -23,3 +23,32 @@ TEST(graph_test,add_edge){
     ASSERT_TRUE(graph.has_edge(3,4));
     ASSERT_FALSE(graph.has_edge({3,4,5.0}));
 }
+
+TEST(graph_test,remove_edge){
+    Graph<int,double> graph;
+    graph.add_vertex(3);
+    graph.add_vertex(4);
+    graph.add_vertex(5);
+    graph.add_edge(3,4,6.0);
+    graph.add_edge(3,5,6.0);
+    ASSERT_TRUE(graph.has_edge(3,4));
+    ASSERT_TRUE(graph.has_edge({3,5,6.0}));
+    ASSERT_FALSE(graph.remove_edge({3,4,5.0}));
+    ASSERT_TRUE(graph.remove_edge(3,5));
+}
+
+TEST(graph_test,order){
+    Graph<int,double> graph;
+    graph.add_vertex(3);
+    graph.add_vertex(4);
+    graph.add_vertex(5);
+    ASSERT_EQ(graph.order(),3);
+}
+
+TEST(graph_test,degree){
+    Graph<int,double> graph;
+    graph.add_vertex(3);
+    graph.add_vertex(4);
+    graph.add_edge(3,4,6.0);
+    ASSERT_EQ(graph.degree(3),1);
+}
